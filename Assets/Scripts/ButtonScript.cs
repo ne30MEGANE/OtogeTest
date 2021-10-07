3c622032ef4e12ff659e18aba3f40a6d06105405
Takahashi <megane.nt5mf@gmail.com>
2021-10-08 02:01:21 +0900

    テスト端末用アス比に対応
    (cherry picked from commit 52e5687e1488e47a1dfe3a7978b07562aff09003)

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public string sceneName;
+
+    public void start()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
